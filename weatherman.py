30130f17b317ca21d7e8b7f1faeb6e94b82e0fb3
Muhammad Tayayb Tahir Qureshi <tayyab.tahir@arbisoft.com>
2024-06-12 11:37:23 +0500

    weather report generation, given a month

--- weatherman.py
+++ weatherman.py
@@ -4,18 +4,18 @@ import os
 import pandas as pd
 
 MONTHS = {
-    1: "Jan",
-    2: "Feb",
-    3: "Mar",
-    4: "Apr",
+    1: "January",
+    2: "February",
+    3: "March",
+    4: "April",
     5: "May",
-    6: "Jun",
-    7: "Jul",
-    8: "Aug",
-    9: "Sep",
-    10: "Oct",
-    11: "Nov",
-    12: "Dec",
+    6: "June",
+    7: "July",
+    8: "August",
+    9: "September",
+    10: "October",
+    11: "November",
+    12: "December",
 }
 
 
@@ -77,7 +77,9 @@ def stats_extremes(weather_readings, year):
             - 'date_max_temp': The date (PKT format) with the maximum temperature.
             - 'date_min_temp': The date (PKT format) with the minimum temperature.
             - 'date_max_humid': The date (PKT format) with the maximum humidity.
+        None: If no data is found for the specified year and month.
     """
+    record_found = False
     max_temperature = -100
     min_temperature = 100
     max_humidity = 0
@@ -87,6 +89,7 @@ def stats_extremes(weather_readings, year):
 
     for key in weather_readings.keys():
         if key.startswith(str(year)):
+            record_found = True
             df = weather_readings[key]
 
             max_temp_index = df["Max TemperatureC"].idxmax()
@@ -94,63 +97,123 @@ def stats_extremes(weather_readings, year):
             temperature = max_temp_row["Max TemperatureC"]
             if temperature > max_temperature:
                 max_temperature = temperature
-                date_max_temp = max_temp_row["PKT"]
+                try:
+                    date_max_temp = max_temp_row["PKT"]
+                except:
+                    date_max_temp = max_temp_row["PKST"]
 
             min_temp_index = df["Min TemperatureC"].idxmin()
             min_temp_row = df.loc[min_temp_index]
             temperature = min_temp_row["Min TemperatureC"]
             if temperature < min_temperature:
                 min_temperature = temperature
-                date_min_temp = min_temp_row["PKT"]
+                try:
+                    date_min_temp = min_temp_row["PKT"]
+                except:
+                    date_min_temp = min_temp_row["PKST"]
 
             max_humid_index = df["Max Humidity"].idxmax()
             max_humid_row = df.loc[max_humid_index]
             humidity = max_humid_row["Max Humidity"]
             if humidity > max_humidity:
                 max_humidity = humidity
-                date_max_humid = max_humid_row["PKT"]
+                try:
+                    date_max_humid = max_humid_row["PKT"]
+                except:
+                    date_max_humid = max_humid_row["PKST"]
 
-    results = {
-        "max_temperature": max_temperature,
-        "min_temperature": min_temperature,
-        "max_humidity": max_humidity,
-        "date_max_temp": date_max_temp,
-        "date_min_temp": date_min_temp,
-        "date_max_humid": date_max_humid,
-    }
-    return results
+    if record_found:
+        results = {
+            "max_temperature": max_temperature,
+            "min_temperature": min_temperature,
+            "max_humidity": max_humidity,
+            "date_max_temp": date_max_temp,
+            "date_min_temp": date_min_temp,
+            "date_max_humid": date_max_humid,
+        }
+        return results
+    else:
+        return None
 
 
 def generate_report_extremes(weather_readings, year):
-    """Generates a report summarizing weather extremes for a year.
+    """Generates a report summarizing weather extremes for a year."""
 
-    This function takes the results of `stats_extremes` for a given year and
-    formats them into a well-defined report.
+    extremes = stats_extremes(weather_readings, year)
+    print(f"\n***** Report for the year {year}: *****")
+    if extremes is not None:
+        date_max_temp = extremes["date_max_temp"].split("-")
+        date_min_temp = extremes["date_min_temp"].split("-")
+        date_humidity = extremes["date_max_humid"].split("-")
+
+        print(
+            f"Highest: {extremes['max_temperature']}C on {MONTHS[int(date_max_temp[1])]} {date_max_temp[2]}"
+        )
+        print(
+            f"Lowest: {extremes['min_temperature']}C on {MONTHS[int(date_min_temp[1])]} {date_min_temp[2]}"
+        )
+        print(
+            f"Humidity: {extremes['max_humidity']}% on {MONTHS[int(date_humidity[1])]} {date_humidity[2]}"
+        )
+    else:
+        print("Sorry! No records found against your input")
+    print("-------------------------------------")
+
+
+def stats_averages(weather_readings, year, month):
+    """Calculates average highest temperature, average lowest temperature,
+    and average mean humidity for a given year and month.
 
     Args:
         weather_readings (dict): A dictionary where readings are stored as
             pandas dataframes.
-        year (int): The year for which the report is generated.
+        year (int): The year for which to calculate averages.
+        month (int): The month (1-12) for which to calculate averages.
 
     Returns:
-        None
+        dict: A dictionary containing the following keys if data is found for the
+            specified year and month:
+            - 'avg_highest_temp': Average of the daily maximum temperatures in °C.
+            - 'avg_lowest_temp': Average of the daily minimum temperatures in °C.
+            - 'avg_mean_humidity': Average of the daily mean humidity values.
+        None: If no data is found for the specified year and month.
     """
-    extremes = stats_extremes(weather_readings, year)
-    date_max_temp = extremes["date_max_temp"].split("-")
-    date_min_temp = extremes["date_min_temp"].split("-")
-    date_humidity = extremes["date_max_humid"].split("-")
+    record_found = False
+    mon = MONTHS[month][:3]
+    year_month = str(year) + "_" + mon
+    for key in weather_readings.keys():
+        if key == year_month:
+            record_found = True
+            avg_highest_temp = weather_readings[key]["Max TemperatureC"].mean()
+            avg_lowest_temp = weather_readings[key]["Min TemperatureC"].mean()
+            avg_mean_humidity = weather_readings[key][" Mean Humidity"].mean()
+            break
 
-    print(
-        f"""Highest: {extremes["max_temperature"]}C on {MONTHS[int(date_max_temp[1])]} {date_max_temp[2]}\nLowest: {extremes["min_temperature"]}C on {MONTHS[int(date_min_temp[1])]} {date_min_temp[2]}\nHumidity: {extremes["max_humidity"]}% on {MONTHS[int(date_humidity[1])]} {date_humidity[2]}
-        """
-    )
+    if record_found:
+        results = {
+            "avg_highest_temp": avg_highest_temp,
+            "avg_lowest_temp": avg_lowest_temp,
+            "avg_mean_humidity": avg_mean_humidity,
+        }
+        return results
+    return None
 
 
-def stats_averages(weather_readings, year, month):
-    pass
+def generate_report_averages(weather_readings, year, month):
+    """Generates a report summarizing average weather statistics for a given year and month."""
+
+    averages = stats_averages(weather_readings, year, month)
+    print(f"\n***** Report for the month: {MONTHS[month]} {year} *****")
+    if averages is not None:
+        print(f"Highest Average: {averages['avg_highest_temp']}C")
+        print(f"Lowest Average: {averages['avg_lowest_temp']}C")
+        print(f"Average Mean Humidity: {averages['avg_mean_humidity']}%")
+    else:
+        print("Sorry! No records found against your input")
+    print("-------------------------------------")
 
 
-def stats_chart(weather_readings, year, month):
+def generate_chart(weather_readings, year, month):
     pass
 
 
@@ -166,9 +229,6 @@ if __name__ == "__main__":
 
     extract_files(args.extract_path)
     weather_readings = parse(args.extract_path)
-    # print(weather_readings["2006_Apr"].head())
-    # cols = weather_readings["2006_Apr"].columns
-    # print(cols)
 
     if args.extremes is not None:
         year = int(args.extremes)
@@ -178,10 +238,10 @@ if __name__ == "__main__":
         inp = args.averages.split("/")
         year = int(inp[0])
         month = int(inp[1])
-        stats_averages(weather_readings, year, month)
+        generate_report_averages(weather_readings, year, month)
 
     if args.chart is not None:
         inp = args.averages.split("/")
         year = int(inp[0])
         month = int(inp[1])
-        stats_chart(weather_readings, year, month)
+        generate_chart(weather_readings, year, month)
