6f2c4b5a1bdecc27fab75a9c7b9eb714a6baad70
Muhammad Tayayb Tahir Qureshi <tayyab.tahir@arbisoft.com>
2024-06-24 11:20:01 +0500

    Allowing multiple dates to be passed

--- weatherman.py
+++ weatherman.py
@@ -7,9 +7,9 @@ from code_files.report_calculator import ReportCalculator
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("extract_to", type=str, help='Directory to extract the results to.')
-    parser.add_argument("-e", "--yearly_report", type=lambda e: datetime.strptime(e, '%Y'), help='Extreme weather stats for a year (format: YYYY).')
-    parser.add_argument("-a", "--monthly_report", type=lambda a: datetime.strptime(a, '%Y/%m'), help='Year & Month for average weather stats (format: YYYY/MM)')
-    parser.add_argument("-c", "--temp_chart", type=lambda c: datetime.strptime(c, '%Y/%m'), help='Year & Month for daily temperature chart (format: YYYY/MM)')
+    parser.add_argument("-e", "--yearly_report", type=lambda e: datetime.strptime(e, '%Y'), nargs='+', help='Extreme weather stats for a year (format: YYYY).')
+    parser.add_argument("-a", "--monthly_report", type=lambda a: datetime.strptime(a, '%Y/%m'), nargs='+', help='Year & Month for average weather stats (format: YYYY/MM)')
+    parser.add_argument("-c", "--temp_chart", type=lambda c: datetime.strptime(c, '%Y/%m'), nargs='+', help='Year & Month for daily temperature chart (format: YYYY/MM)')
     parser.add_argument("--inline", action="store_true", help="One bar chart for highest and lowest temp on each day")
 
     args = parser.parse_args()
@@ -19,46 +19,50 @@ if __name__ == "__main__":
     parser.parse_all_files()
 
     if args.yearly_report:
-        yearly_readings = [
-            r for r in parser.weather_readings if r.date.startswith(args.yearly_report.strftime('%Y'))
-        ]
-        if yearly_readings:
-            weather_extremes = ReportCalculator.compute_extreme_stats(yearly_readings)
-            WeatherReporter.generate_report_extremes(weather_extremes)
-        else:
-            print(f"No record found to show WEATHER EXTREMES agaisnt your input")
+        for yearly_report in args.yearly_report:
+            yearly_readings = [
+                r for r in parser.weather_readings if r.date.startswith(yearly_report.strftime('%Y'))
+            ]
+            if yearly_readings:
+                weather_extremes = ReportCalculator.compute_extreme_stats(yearly_readings)
+                WeatherReporter.generate_report_extremes(weather_extremes)
+            else:
+                print(f"No record found to show WEATHER EXTREMES agaisnt your input")
 
     if args.monthly_report:
-        year = args.monthly_report.strftime('%Y')
-        month = int(args.monthly_report.strftime('%m'))
-        monthly_readings = [
-            r for r in parser.weather_readings if r.date.startswith(f"{year}-{month}")
-        ]
-        if monthly_readings:
-            avg_max_temp, avg_min_temp, avg_mean_humidity = (
-                ReportCalculator.compute_average_stats(monthly_readings)
-            )
-            WeatherReporter.generate_report_averages(
-                year, month, avg_max_temp, avg_min_temp, avg_mean_humidity
-            )
-        else:
-            print(f"No record found to show AVERAGE STATS against your input")
+        for monthly_report in args.monthly_report:
+            year = monthly_report.strftime('%Y')
+            month = int(monthly_report.strftime('%m'))
+            monthly_readings = [
+                r for r in parser.weather_readings if r.date.startswith(f"{year}-{month}-")
+            ]
+            if monthly_readings:
+                avg_max_temp, avg_min_temp, avg_mean_humidity = (
+                    ReportCalculator.compute_average_stats(monthly_readings)
+                )
+                WeatherReporter.generate_report_averages(
+                    year, month, avg_max_temp, avg_min_temp, avg_mean_humidity
+                )
+            else:
+                print(f"No record found to show AVERAGE STATS against your input")
 
     if args.temp_chart:
-        year = args.temp_chart.strftime('%Y')
-        month = int(args.temp_chart.strftime('%m'))
-        monthly_readings = [
-            r for r in parser.weather_readings if r.date.startswith(f"{year}-{month}")
-        ]
+        for temp_chart in args.temp_chart:
+            year = temp_chart.strftime('%Y')
+            month = int(temp_chart.strftime('%m'))
+            monthly_readings = [
+                r for r in parser.weather_readings if r.date.startswith(f"{year}-{month}-")
+            ]
 
-        if monthly_readings:
-            for reading in monthly_readings:
-                if reading.max_temp and reading.min_temp:
-                    high_bar = "+" * reading.max_temp
-                    low_bar = "+" * reading.min_temp
-                    date = reading.date.split("-")
-                    WeatherReporter.generate_report_barchart(
-                        date, high_bar, low_bar, args.inline
-                    )
-        else:
-            print(f"No record found to plot CHARTS against your input")
+            if monthly_readings:
+                for reading in monthly_readings:
+                    if reading.max_temp and reading.min_temp:
+                        high_bar = "+" * reading.max_temp
+                        low_bar = "+" * reading.min_temp
+                        date = reading.date.split("-")
+                        WeatherReporter.generate_report_barchart(
+                            date, high_bar, low_bar, args.inline
+                        )
+            else:
+                print(f"No record found to plot CHARTS against your input")
+            print("-------------------------------------")
