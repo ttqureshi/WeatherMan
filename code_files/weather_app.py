db534a781912fa42dc9c565925d41bfdd97b5019
Muhammad Tayayb Tahir Qureshi <tayyab.tahir@arbisoft.com>
2024-06-26 11:07:52 +0500

    Organizing imports

--- code_files/weather_app.py
+++ code_files/weather_app.py
@@ -1,5 +1,12 @@
 from code_files.weather_parser import WeatherParser
 from code_files.weather_reporter import WeatherReporter
 from code_files.report_calculator import ReportCalculator
+from code_files.weather_readings import WeatherReading, WeatherExtremes
 
-__all__ = ['WeatherParser', 'WeatherReporter', 'ReportCalculator']
+__all__ = [
+    "WeatherParser",
+    "WeatherReporter",
+    "ReportCalculator",
+    "WeatherReading",
+    "WeatherExtremes",
+]
