5618bcf64b676d8a920a601ccd7acb117c135d8b
Muhammad Tayayb Tahir Qureshi <tayyab.tahir@arbisoft.com>
2024-07-19 12:30:51 +0500

    feat: Search Products
    - View to filter products based on the search query
    - Write template to show search results

--- eCommercePlatform/products/urls.py
+++ eCommercePlatform/products/urls.py
@@ -7,4 +7,5 @@ app_name = "products"
 urlpatterns = [
     path("", views.products_listing_view, name="products-listing"),
     path("<int:product_id>/", views.product_detail_view, name="detail"),
+    path("search/", views.search, name="search"),
 ]
--- eCommercePlatform/products/views.py
+++ eCommercePlatform/products/views.py
@@ -29,3 +29,12 @@ def products_listing_view(request):
 def product_detail_view(request, product_id):
     product = Product.objects.get(pk=product_id)
     return render(request, "products/product_detail.html", {"product": product})
+
+
+def search(request):
+    search_query = request.GET.get('search')
+    search_results = Product.objects.filter(description__icontains=search_query)
+    return render(request, "products/search.html", {
+        "search_query": search_query,
+        "search_results": search_results,
+    })
